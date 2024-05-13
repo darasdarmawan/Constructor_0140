6bf967083f3637b4fee31d16502822e342be9f7f
darasdarmawan <darasyauqidarmawan@gmail.com>
2024-05-13 11:09:15 +0700

    menambahkan fungsi main

--- StaticFunction/StaticFunction.cpp
+++ StaticFunction/StaticFunction.cpp
@@ -5,9 +5,9 @@ using namespace std;
 class mahasiswa
 {
 private:
-	static int nim;
+	long long static int nim;
 public: 
-	int id;
+	long long int id;
 	string nama;
 
 	void setID();
@@ -16,6 +16,40 @@ public:
 	static void setNim(int pNim) { nim = pNim; }
 	static int getNim() { return nim; }
 
-	mahasiswa(string pnama) : nama(pnama) { setID() }
+	mahasiswa(string pnama)
+	{
+		nama = pnama;
+		setID();
+	}
 };
+
+long long int mahasiswa::nim = 20230140140;
+
+void mahasiswa::setID()
+{
+	id = ++nim;
+}
+
+void mahasiswa::printAll()
+{
+	cout << "ID = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
+}
+
+int main()
+{
+	mahasiswa mhs1("Dara SyauqI Darmawan");
+	mahasiswa mhs2("Moza");
+	mahasiswa::setNim(9);
+	mahasiswa mhs3("Sekar");
+	mahasiswa mhs4("Kamilia");
+
+	mhs1.printAll();
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
+
+	cout << "akses dari luar"
+}
 
